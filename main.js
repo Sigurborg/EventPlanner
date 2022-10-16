88205f246a0b3bf193e3ca06cf9ee934c8bd06d8
Sigurborg <sigselma@gmail.com>
2022-10-16 16:52:09 +0000

    css create event

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ const generateEventList = () => {
     events.forEach((event, index) => {
       const cardElement = document.createElement("div");
       const otherElements = document.createElement("a");
-  
+
       const titleElement = document.createElement("h1");
       const categoryElement = document.createElement("p");
       const attendingElement = document.createElement("p");
@@ -44,13 +44,12 @@ const generateEventList = () => {
       const ownerElement = document.createElement("p");
       const attendBtnElement = document.createElement("button");
       const imageElement = document.createElement("img");
-  
+
       const name = localStorage.getItem("userName");
       if (event.Attending.includes(name)) {
         attendBtnElement.innerText = "Going";
       } else {
         attendBtnElement.innerText = "Join";
-  
       }
       titleElement.innerText = event.Title;
       categoryElement.innerText = event.Category;
@@ -60,7 +59,7 @@ const generateEventList = () => {
         " - " +
         new Date(event.Ending).toLocaleTimeString("is", timeFormat);
       ownerElement.innerText = "Added by " + event.Owner;
-  
+
       otherElements.href = "about-event/about-event.html?eventid=" + event._id;
       if (event.Category.toLowerCase() === "conference") {
         imageElement.src = "images/conference.jpg";
@@ -83,60 +82,57 @@ const generateEventList = () => {
       startDateElement.classList.add("card-startdate");
       ownerElement.classList.add("card-owner");
       cardElement.classList.add("card");
-  
+
       // Use this class ("first-card") to style the top event
       if (index === 0) {
         cardElement.classList.add("first-card");
       }
-  
+
       attendBtnElement.classList.add("card-button");
-  
+
       otherElements.appendChild(imageElement);
       otherElements.appendChild(titleElement);
       otherElements.appendChild(categoryElement);
       otherElements.appendChild(attendingElement);
       otherElements.appendChild(startDateElement);
       otherElements.appendChild(ownerElement);
-  
+
       cardElement.appendChild(otherElements);
       cardElement.appendChild(attendBtnElement);
-  
+
       eventList.appendChild(cardElement);
-  
+
       // Attend button
       attendBtnElement.setAttribute("data-events_id", event._id);
-  
+
       attendBtnElement.onclick = function clickAttend() {
         const name = localStorage.getItem("userName");
-  
+
         if (event.Attending.includes(name)) {
-          console.log('here');
-            const updatedAddendingList = event.Attending.filter((listName) => {
-              console.log('trying to remove');
-              // if you're it, you're removed
-              if (listName === name) {
-                return false;
-              }
-              // everyone else can come
-              return true;
-            });
-            
-            event.Attending = updatedAddendingList;
-  
-            attendBtnElement.innerText = "Join";
-            attendBtnElement.style.backgroundColor = "#5E9991";
-            updateEvent(event._id, event);
-            /*
+          console.log("here");
+          const updatedAddendingList = event.Attending.filter((listName) => {
+            console.log("trying to remove");
+            // if you're it, you're removed
+            if (listName === name) {
+              return false;
+            }
+            // everyone else can come
+            return true;
+          });
+
+          event.Attending = updatedAddendingList;
+
+          attendBtnElement.innerText = "Join";
+          updateEvent(event._id, event);
+          /*
             remove the user from the array of attending
             reset the button to original state
             run updateEvent method from api
           */
         } else {
-          console.log('trying to add');
+          console.log("trying to add");
           event.Attending.push(name);
           updateEvent(event._id, event);
-    
-          attendBtnElement.style.backgroundColor = "#5E9991";
           attendBtnElement.innerText = "Going";
         }
       };
@@ -147,17 +143,16 @@ const generateEventList = () => {
       if (i < 3) return;
       deleteEvent(event._id);
     });*/
-  
+
     console.log(events);
   });
-}
+};
 
 // called once on opening the page
 generateEventList();
 
-
 // Event list on main page
-setInterval (()=>{
+setInterval(() => {
   // called once every five seconds
   generateEventList();
-},5000)
+}, 5000);
